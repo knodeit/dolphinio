29e9936d5857bc710c1ee627ee2e7ec30251a7f4
Vadim <vadim@knodeit.com>
2015-08-21 20:01:55 +0300

    finished acl

--- modules/acl.js
+++ modules/acl.js
@@ -3,10 +3,15 @@
 /**
  * Module dependencies.
  */
+var Q = require('q');
 
 module.exports = function (database) {
     var Schema = database.Schema;
     var AclSchema = new Schema({
+        module: {
+            type: String,
+            default: ''
+        },
         role: {
             type: String,
             default: ''
@@ -21,5 +26,26 @@ module.exports = function (database) {
     });
 
     AclSchema.index({role: 1, entity: 1}, {unique: 1});
+
+    AclSchema.statics.updateRow = function (_id, permissions) {
+        var deferred = Q.defer();
+        var Acl = database.connection.model('Acl');
+        Acl.findOne({_id: _id}).exec(function (err, row) {
+            if (!row) {
+                return deferred.reject(new Error('Row not found'));
+            }
+
+            row.permissions = permissions;
+            row.save(function (err, row) {
+                if (err) {
+                    console.error(err);
+                    return deferred.reject(new Error('Mongo error'));
+                }
+
+                deferred.resolve(row);
+            });
+        });
+        return deferred.promise;
+    };
     database.connection.model('Acl', AclSchema);
 };
